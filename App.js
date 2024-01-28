e09d4a3fe6183c29fc05b9500d5f599087859e39
dineth panditha <dinethpanditha@dineths-MacBook-Pro.local>
2024-01-28 13:13:00 +0530

    did some styles for input field

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @format
  */
 
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { ImageBackground, StyleSheet, TextInput, View } from "react-native";
 
 
@@ -14,6 +14,8 @@ function App() {
   const [input, setInput] = useState("");
   const [loading, setLoading] = useState("");
   const [data, setData] = useState([]);
+
+  const fetchDataHandler=useCallback(()=>{},[]);
 
   return (<View style={styles.root}>
       <ImageBackground source={require("./assects/bg.jpg")}
@@ -40,6 +42,17 @@ const styles = StyleSheet.create({
   }, image: {
     flex: 1, flexDirection: "column",
   },
+  textInput:{
+    borderBottomWidth:0,
+    padding:5,
+    paddingVertical:20,
+    marginVertical:100,
+    marginHorizontal:20,
+    backgroundColor:'#fff',
+    fontSize:20,
+    borderRadius:20
+
+  }
 });
 
 export default App;
